f0bf73b59c46c544c4f25a37c0a3e44e75ed56d4
jedster1111 <jedster1111@hotmail.co.uk>
2019-06-28 01:25:15 +0100

    Trying out tsc instead of rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
 
 import pkg from './package.json';
 
@@ -22,19 +19,8 @@ export default {
         },
     ],
     plugins: [
-        external(),
-        resolve(),
         typescript({
-            rollupCommonJSResolveHack: true,
-            exclude: '**/__tests__/**',
-            clean: true,
-        }),
-        commonjs({
-            include: ['node_modules/**'],
-            namedExports: {
-                'node_modules/react/react.js': ['Children', 'Component', 'PropTypes', 'createElement'],
-                'node_modules/react-dom/index.js': ['render'],
-            },
-        }),
+            exclude: '**/__tests__/**'
+        })
     ],
 };
